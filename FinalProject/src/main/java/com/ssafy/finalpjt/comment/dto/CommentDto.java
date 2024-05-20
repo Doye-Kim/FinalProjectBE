0cff3b7e6e170309d08bdd9c79aba32d9ce532a9
Hyeongjun Lee <123728524+hhyeongjun@users.noreply.github.com>
2024-05-20 11:07:54 +0900

    Update CommentDto.java

--- FinalProject/src/main/java/com/ssafy/finalpjt/comment/dto/CommentDto.java
+++ FinalProject/src/main/java/com/ssafy/finalpjt/comment/dto/CommentDto.java
@@ -2,6 +2,10 @@ package com.ssafy.finalpjt.comment.dto;
 
 import java.time.LocalDateTime;
 
+import com.ssafy.finalpjt.post.dto.PostDto;
+
+import lombok.NoArgsConstructor;
+
 
 public class CommentDto {
 
@@ -10,56 +14,54 @@ public class CommentDto {
     private String commentContent;
     private LocalDateTime commentTime;
     
-    private int userSeq;
+    // 외래키
     private int postSeq;
-
-    CommentDto(){}
-	public CommentDto(int postSeq, String commentContent, LocalDateTime commentTime) {
+    private int userSeq;
+    
+    
+	public CommentDto(String commentContent, LocalDateTime commentTime, int postSeq, int userSeq) {
 		super();
-		this.postSeq = postSeq;
 		this.commentContent = commentContent;
 		this.commentTime = commentTime;
 		this.postSeq = postSeq;
+		this.userSeq = userSeq;
 	}
-
 	public int getCommentSeq() {
 		return commentSeq;
 	}
-
 	public void setCommentSeq(int commentSeq) {
 		this.commentSeq = commentSeq;
 	}
-
 	public String getCommentContent() {
 		return commentContent;
 	}
-
 	public void setCommentContent(String commentContent) {
 		this.commentContent = commentContent;
 	}
-
 	public LocalDateTime getCommentTime() {
 		return commentTime;
 	}
-
 	public void setCommentTime(LocalDateTime commentTime) {
 		this.commentTime = commentTime;
 	}
-
 	public int getPostSeq() {
 		return postSeq;
 	}
-
 	public void setPostSeq(int postSeq) {
 		this.postSeq = postSeq;
 	}
-
+	public int getUserSeq() {
+		return userSeq;
+	}
+	public void setUserSeq(int userSeq) {
+		this.userSeq = userSeq;
+	}
 	@Override
 	public String toString() {
 		return "CommentDto [commentSeq=" + commentSeq + ", commentContent=" + commentContent + ", commentTime="
-				+ commentTime + ", postSeq=" + postSeq + "]";
+				+ commentTime + ", postSeq=" + postSeq + ", userSeq=" + userSeq + "]";
 	}
-    
+
 	
     
 }
